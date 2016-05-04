934cf6443b11d03876c408a4e3c6536de52fc80b
Mateusz Koziestański <matico575@gmail.com>
2016-05-04 18:56:51 +0200

    Dzień 3: C wciąż nie wie że powinno działać
    <smutna_rzaba.jpg>

--- Warunki/Source.c
+++ Warunki/Source.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#define _WYDZIALY_ 18
-#define MAX_KIERUNKI 7
-#define MAX_PRZEDMIOTY 22
-#define _SEMESTRY_ 7
+#define WYDZIALOW 18
+#define KOLUMN 7
+
 
 const char* getfield(char* line, int num)
 //metoda pobieraj¹ca treœæ pola w CSV o wskazanym numerze  
@@ -30,6 +29,7 @@ void read_csv(FILE* file) {
 	}
 }
 
+
 int is_in_array(char val, char *arr, int size) {
 	int i;
 	for (i = 0; i < size; i++) {
@@ -41,18 +41,19 @@ int is_in_array(char val, char *arr, int size) {
 
 int main()
 {
-	char wybor;
-	char wydzial[_WYDZIALY_]; //(indeks+1) to IdWydzia³u, przechowywane maj¹ byæ Nazwy
-	char przedmiot[_WYDZIALY_][MAX_KIERUNKI][_SEMESTRY_][MAX_PRZEDMIOTY]; 
+	char wybor, line[512];
+	 //(indeks+1) to IdWydzia³u, przechowywane maj¹ byæ Nazwy
+	char przedmiot[3000][KOLUMN], wydzial[WYDZIALOW];
+	
 	/*Nie mam pomys³u jak inaczej, ale mo¿e takimi tablicami warto. 
 	Ogó³em chodzi ¿eby to co zczytywane jest z ka¿dej csv, np z wydzia³y, by³o zapisywane,
 	¿eby siê potem da³o robiæ coœ pokroju wielopoziomowego wybierania:
 		if(strtoi(wybor) < _WYDZIALY_)
 			>>tu wstawiæ dalsze dzia³ania, które bêd¹ wyœwietlaæ dalsze poziomy w oparciu o podane IdWydzia³u<<
 	*/
-
-	
+	int lines = 0;
 	FILE* wydzialy = fopen("wydzialy.csv", "r");
+	
 	FILE* przedmioty = fopen("Przedmioty.csv", "r");
 
 	printf("\t \t=================WARUNKOLICZNIK====================\n");
@@ -61,9 +62,25 @@ int main()
 	printf("Przedmioty dotyczace j. obcych lub WF zostaly przeniesione odpowiednio pod nr 17 i 18. \n");
 	printf("\t \t Wybierz wydzial z listy: \n");
 
-	read_csv(wydzialy);
-	printf("Nr: ");
-	scanf("%c", &wybor);
+	while (fgets(line, 512, wydzialy))
+	{
+		char* tmp = strdup(line);
+		const char* field1 = getfield(tmp, 1);
+		free(tmp);
+		tmp = strdup(line);
+		const char* field2 = getfield(tmp, 2);
+		printf("%s: %s \n", field1, field2 );
+		tmp = strdup(line);
+		field2 = getfield(tmp, 2);
+		wydzial[lines] = field2;
+		lines++;
+		
+	}
+	
+	//read_csv(wydzialy);
+
+	//printf("Nr: ");
+	//scanf("%c", &wybor);
 
 	system("pause");
 	return 0;
